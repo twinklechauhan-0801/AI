9cf02a7bea502b1df20a0743516cdd6dc5b4592b
jaanviverma-0803 <jaanviverma0819@gmail.com>
2026-06-29 10:16:25 +0530

    1st program

--- age.py
+++ age.py
@@ -1,5 +1,22 @@
-age=30
-if age>18:
-    print("eligible")
-else:
-    print("not eligible")
+name="twinkle"
+def function():
+   global name
+   name="mahak"
+   print("hello my name is ",name)
+function()
+print(name)
+num=10.5
+x=int(num)
+print(x)
+y="hello  all"
+print(len(y))
+z="hello world"
+print(z[3:8])
+
+message=" hello class myself mahak "
+print(message[-8:-3])
+print(message.lower())
+print(message.upper())
+message=message.replace("mahak","twinkle")
+message=message.strip()
+print(message)
---
+++ new.py
@@ -0,0 +1,9 @@
+message="Hello world this is my python programming class"
+print(message.upper())
+print(len(message))
+text=" I am here to Learn Python Programming "
+print(text.strip().lower().replace("Python","PYTHON"))
+name="mahak","jaanvi","jiya","twinkle"
+print(name)
+print(message[-7:-1])
+print("java" not  in message)
